28e6d4113235c1670f1e319f3dd1dece0d73bec6
Kwaghuter Raphael <raphaelgbaorun@gmail.com>
2025-11-09 23:40:13 +0100

    feat

--- app.py
+++ app.py
@@ -45,6 +45,6 @@ def predict():
     return render_template('index.html', prediction=emotion, image=img_path)
 
 if __name__ == '__main__':
-    import os
-    port = 8000
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
+
